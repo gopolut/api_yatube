635287f4b4f612283788a563eedadf9c26b8f108
Лютиченко Павел <motry@yandex.ru>
2021-12-14 18:01:57 +0300

    удалили неиспользуемые файлы, поправил urls.py

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -6,17 +6,17 @@ from rest_framework.authtoken import views
 from .views import CommentViewSet, GroupViewSet, PostsViewSet, UserViewSet
 
 router = routers.DefaultRouter()
-router.register(r'api/v1/users', UserViewSet)
-router.register(r'api/v1/posts', PostsViewSet)
-router.register(r'api/v1/groups', GroupViewSet)
+router.register(r'users', UserViewSet)
+router.register(r'posts', PostsViewSet)
+router.register(r'groups', GroupViewSet)
 router.register(
-    r'api/v1/posts/(?P<post_id>\d+)/comments',
+    r'posts/(?P<post_id>\d+)/comments',
     CommentViewSet,
     basename='comment'
 )
 
 
 urlpatterns = [
-    path('api/v1/api-token-auth/', views.obtain_auth_token),
-    path('', include(router.urls))
+    path('v1/api-token-auth/', views.obtain_auth_token),
+    path('v1/', include(router.urls))
 ]
