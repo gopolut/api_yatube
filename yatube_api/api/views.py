7b677e2ef25c274e885b1269449f73d57bc6ea3d
Лютиченко Павел <motry@yandex.ru>
2021-12-15 20:10:45 +0300

    Настроил isort в файле setup.cfg

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import get_object_or_404
+
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 
-from posts.models import Group, Post, User
-# isort здесь добавляет пустую строку
 from .permissions import IsAuthorOrReadOnly
 from .serializers import (CommentSerializer, GroupsSerializer, PostSerializer,
                           UserSerializer)
+from posts.models import Group, Post, User
 
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
